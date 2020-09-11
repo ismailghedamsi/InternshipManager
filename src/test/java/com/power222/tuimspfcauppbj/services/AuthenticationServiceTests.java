dcbf3854f8c41c7fb300bf75b1e64809242f6b36
Simon Longpré-Landry <simonllandry@hotmail.com>
2020-09-11 15:23:33 -0400

    EQ3-45: API d'authentification et configuration Spring Security

--- src/test/java/com/power222/tuimspfcauppbj/services/AuthenticationServiceTests.java
+++ src/test/java/com/power222/tuimspfcauppbj/services/AuthenticationServiceTests.java
@@ -1,36 +1,46 @@
 package com.power222.tuimspfcauppbj.services;
 
-import com.power222.tuimspfcauppbj.dao.UserRepository;
 import com.power222.tuimspfcauppbj.model.User;
 import com.power222.tuimspfcauppbj.service.AuthenticationService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
-
-import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class AuthenticationServiceTests {
 
-    @Mock
-    private UserRepository userRepo;
-
-    @MockBean
+    @Autowired
     private AuthenticationService authSvc;
 
-    @Test
-    @WithMockUser(value = "admin", password = "password", )
-    void invalidUser() {
-        when(userRepo.findByUsername(any())).thenReturn(Optional.empty());
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
+    @Test
+    @WithMockUser("fk_etudiant")
+    void invalidUser() {
         assertThat(authSvc.getCurrentUser(), is(equalTo(new User())));
+    }
+
+    @Test
+    @WithMockUser("etudiant")
+    void validUser() {
+        User expected = User.builder()
+                .id(2L)
+                .username("etudiant")
+                .password(null)
+                .enabled(true)
+                .role("student")
+                .build();
+
+        User actual = authSvc.getCurrentUser();
+        actual.setPassword(null);
+
+        assertThat(actual, is(equalTo(expected)));
     }
 }
