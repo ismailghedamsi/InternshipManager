49fafd6d52f50e198ed7bd2f3a98395f15e60fc3
Simon Longpré-Landry <simonllandry@hotmail.com>
2020-09-16 15:47:04 -0400

    EQ3-45: Ajout de contrainte unique sur username (#21)
    * EQ3-45: Ajout de contrainte unique sur username
    
    * EQ3-45: Nom de commit

--- src/main/java/com/power222/tuimspfcauppbj/model/User.java
+++ src/main/java/com/power222/tuimspfcauppbj/model/User.java
@@ -20,7 +20,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String username;
+
     @JsonIgnore
     private String password;
     private String role;
