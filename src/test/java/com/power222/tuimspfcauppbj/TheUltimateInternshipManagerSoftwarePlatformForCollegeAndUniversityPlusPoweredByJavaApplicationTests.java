7732a3d9cdc572ee86bf09da479198c294b9a529
Simon Longpré-Landry <simonllandry@hotmail.com>
2020-09-20 21:59:34 -0400

    EQ3-3: Conflicts with master (#31)

--- src/test/java/com/power222/tuimspfcauppbj/TheUltimateInternshipManagerSoftwarePlatformForCollegeAndUniversityPlusPoweredByJavaApplicationTests.java
+++ src/test/java/com/power222/tuimspfcauppbj/TheUltimateInternshipManagerSoftwarePlatformForCollegeAndUniversityPlusPoweredByJavaApplicationTests.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TheUltimateInternshipManagerSoftwarePlatformForCollegeAndUniversityPlusPoweredByJavaApplicationTests {
 
@@ -28,7 +27,6 @@ class TheUltimateInternshipManagerSoftwarePlatformForCollegeAndUniversityPlusPow
 		assertThat(response, is(notNullValue()));
 		assertThat(response.getStatusCode(), is (equalTo(HttpStatus.OK)));
 		assertThat(response.getBody(), is(equalTo("Hello, world!")));
-
     }
 
     @Test
@@ -54,5 +52,4 @@ class TheUltimateInternshipManagerSoftwarePlatformForCollegeAndUniversityPlusPow
         assertThat(response, is(notNullValue()));
         assertThat(response.getStatusCode(), is(equalTo(HttpStatus.UNAUTHORIZED)));
     }
-
 }
