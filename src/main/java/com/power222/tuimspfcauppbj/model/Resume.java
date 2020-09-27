f509b6d9602ffc6852e3c99c030eeee185544f8f
Andrei <andrei.belkin.0@protonmail.com>
2020-09-27 12:31:40 -0400

    EQ3-4 : Minor changes

--- src/main/java/com/power222/tuimspfcauppbj/model/Resume.java
+++ src/main/java/com/power222/tuimspfcauppbj/model/Resume.java
@@ -1,24 +1,29 @@
 package com.power222.tuimspfcauppbj.model;
 
-import lombok.*;
-import lombok.experimental.SuperBuilder;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.io.File;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString(callSuper = true)
-@SuperBuilder
-@EqualsAndHashCode
 @Entity
+@Builder
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Resume {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private File file;
+    @Lob
+    private String file;
+
+    private String name;
 
     @ManyToOne
     private Student owner;
---
+++ src/main/java/com/power222/tuimspfcauppbj/service/ResumeService.java
@@ -0,0 +1,7 @@
+package com.power222.tuimspfcauppbj.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResumeService {
+}
