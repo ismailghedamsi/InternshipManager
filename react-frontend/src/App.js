fa5661ff10720a90779471b7a35307f6b7dd9274
Ernesto231 <max2power222gmail.com>
2020-09-28 17:34:25 -0400

    EQ3-56: Form et magnifique

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,26 +1,51 @@
-import React from "react";
 import Login from './Components/Login';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import RegisteringManager from "./Components/RegisteringManager";
 import Welcome from "./Components/Welcome";
 import OffreStage from "./Components/OffreStage/OffreStage";
 import CreateStage from "./Components/OffreStage/CreateStage";
+import CreateStuff from "./Components/OffreStage/CreateStuff";
 
 function App() {
     return (
-        <div className="App">
-            <Router>
-                <Switch>
-                    <Route exact path="/" component={Login}/>
-                    <Route path="/register" component={RegisteringManager}/>
-                    <Route path="/login" component={Login}/>
-                    <Route path="/welcome" component={Welcome}/>
-                    <Route path="/OffreStage" component={OffreStage}/>
-                    <Route path="/CreateStage" component={CreateStage}/>
-                </Switch>
-            </Router>
-        </div>
-    );
+        < div
+    className = "App" >
+        < Router >
+        < Switch >
+        < Route
+    exact
+    path = "/"
+    component = {Login}
+    />
+    < Route
+    path = "/register"
+    component = {RegisteringManager}
+    />
+    < Route
+    path = "/login"
+    component = {Login}
+    />
+    < Route
+    path = "/welcome"
+    component = {Welcome}
+    />
+    < Route
+    path = "/OffreStage"
+    component = {OffreStage}
+    />
+    < Route
+    path = "/CreateStage"
+    component = {CreateStage}
+    />
+    < Route
+    path = "/CreateStuff"
+    component = {CreateStuff}
+    />
+    < /Switch>
+    < /Router>
+    < /div>
+)
+    ;
 }
 
 export default App;
