5963e784e34d400ba89eb9dd7fe8b903b5134a01
Simon Longpré-Landry <simonllandry@hotmail.com>
2020-10-04 00:04:12 -0400

    EQ3-9: Fixed misbehaviour  of authEntrypoint

--- src/main/java/com/power222/tuimspfcauppbj/config/NoPopupBasicAuthenticationEntrypoint.java
+++ src/main/java/com/power222/tuimspfcauppbj/config/NoPopupBasicAuthenticationEntrypoint.java
@@ -10,11 +10,17 @@ import java.io.IOException;
 
 public class NoPopupBasicAuthenticationEntrypoint implements AuthenticationEntryPoint {
 
+    private boolean expiredRaised = false;
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
 
         if (exception instanceof CredentialsExpiredException) {
             response.sendError(498, exception.getMessage());
+        } else if (!expiredRaised) {
+            response.sendError(401, exception.getMessage());
         }
+
+        expiredRaised = exception instanceof CredentialsExpiredException;
     }
 }
