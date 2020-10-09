e8503ac66bc6098c85861f091f487d8c64e6d67d
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2020-10-08 23:09:25 -0400

    EQ3-89 : Add missing offer informations

--- src/main/java/com/power222/tuimspfcauppbj/model/InternshipOffer.java
+++ src/main/java/com/power222/tuimspfcauppbj/model/InternshipOffer.java
@@ -1,11 +1,23 @@
 package com.power222.tuimspfcauppbj.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
-
-import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
@@ -14,20 +26,22 @@ import java.util.List;
 @Builder(toBuilder = true)
 @EqualsAndHashCode
 public class InternshipOffer {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String title;
     private String description;
-    private int nbOfWeeks;
     private double salary;
-    private int beginHour;
-    private int endHour;
     private Date creationDate;
     private Date limitDateToApply;
+    private Date internshipStartDate;
+    private Date internshipEndDate;
+    private int nbStudentToHire;
     private ReviewState reviewState;
     private String reasonForRejection;
+
 
     @Lob
     private String joinedFile;
