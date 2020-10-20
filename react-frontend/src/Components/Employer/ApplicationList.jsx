e01da115992fd4eb347d52578a82a6b3fc416246
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2020-10-20 18:38:35 -0400

    EQ3-17 : Replace button with link to redirect to interview convocation

--- react-frontend/src/Components/Employer/ApplicationList.jsx
+++ react-frontend/src/Components/Employer/ApplicationList.jsx
@@ -65,11 +65,15 @@ export default function ApplicationList() {
                             </Typography>
 
                             <Link variant={"body1"}
-                                  onClick={() => {
-                                      console.log("passed value " + JSON.stringify(offer.applications[i]))
-                                      history.push("/dashboard/interviewConvocation", {...offer.applications[i]})
-                                  }
-                                  }
+                                //   onClick={() => {
+                                //       console.log("passed value " + JSON.stringify(offer.applications[i]))
+                                //       history.push("/dashboard/interviewConvocation", {...offer.applications[i]})
+                                //   }
+                                //   }
+                                  to={{
+                                      pathname: "/dashboard/interviewConvocation",
+                                      state: {...offer.applications[i]}
+                                  }}
                             >
                                 Convoquer l'étudiant pour un entrevue
                             </Link>
