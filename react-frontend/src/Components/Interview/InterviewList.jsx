26d32f2d304f4f997b6753f2582c85bbc9906d02
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2020-10-20 23:34:00 -0400

    EQ3-17 : Interview confirmation state

--- react-frontend/src/Components/Interview/InterviewList.jsx
+++ react-frontend/src/Components/Interview/InterviewList.jsx
@@ -32,6 +32,15 @@ export default function Interviewlist(props) {
         history.push("/dashboard/rescheduleInterview", {...interview})
     }
 
+    function isInterviewAccepted(interview) {
+        if (interview.reviewState == "APPROVED") {
+            return "L'étudiant a accepté l'entrevue"
+        } else if (interview.reviewState == "DENIED") {
+            return "L'étudiant a refusé  l'entrevue"
+        }
+        return "En attente d'approbation"
+    }
+
     return (
         <div className={classes.viewbox}>
             <Container className={classes.container}>
@@ -46,14 +55,15 @@ export default function Interviewlist(props) {
                                 : {interview.studentApplication ? interview.studentApplication.offer.title : ""}</Typography>
                             {<Typography>Etudiant à entrevoir
                                 : {interview.studentApplication ? interview.studentApplication.student.firstName + " " + interview.studentApplication.student.lastName : ""}</Typography>}
+                            <Typography>{isInterviewAccepted(interview)}</Typography>
                             <Button onClick={() => {
-                            const interviewToDeleteIndex = interviews.findIndex(interv => interv.id === interview.id);
-                            const copyInterviews = [...interviews]
-                            api.delete("/interviews/" + interview.id)
-                                .then(() => {
-                                    copyInterviews.splice(interviewToDeleteIndex, 1)
-                                    setInterviews(copyInterviews)
-                                })
+                                const interviewToDeleteIndex = interviews.findIndex(interv => interv.id === interview.id);
+                                const copyInterviews = [...interviews]
+                                api.delete("/interviews/" + interview.id)
+                                    .then(() => {
+                                        copyInterviews.splice(interviewToDeleteIndex, 1)
+                                        setInterviews(copyInterviews)
+                                    })
                             }}>Supprimer</Button>
                             <Button onClick={() => {
                                 redirectEditFormInterview(interview);
