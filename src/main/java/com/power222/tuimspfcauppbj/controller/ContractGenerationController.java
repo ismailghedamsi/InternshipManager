b004c1180dfb1e03a4c1d226460dcccc93c85939
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2020-10-24 18:44:45 -0400

    EQ3-121 : Controller to test contract pdf generation

---
+++ src/main/java/com/power222/tuimspfcauppbj/controller/ContractGenerationController.java
@@ -0,0 +1,29 @@
+package com.power222.tuimspfcauppbj.controller;
+
+import com.power222.tuimspfcauppbj.model.Contract;
+import com.power222.tuimspfcauppbj.service.ContractGenerationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileNotFoundException;
+
+@RestController
+@RequestMapping("/api/contractGeneration")
+public class ContractGenerationController {
+    private ContractGenerationService service;
+
+    public ContractGenerationController(ContractGenerationService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public void generateContract(@RequestBody Contract contract) {
+        try {
+            service.generateContract(contract);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
