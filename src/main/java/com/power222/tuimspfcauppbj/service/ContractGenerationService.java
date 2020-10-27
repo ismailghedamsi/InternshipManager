50e8bce0647f57bde61b42dd183bc1d50dadb0b3
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2020-10-26 21:19:14 -0400

    EQ3-121 : Save contract

--- src/main/java/com/power222/tuimspfcauppbj/service/ContractGenerationService.java
+++ src/main/java/com/power222/tuimspfcauppbj/service/ContractGenerationService.java
@@ -20,20 +20,25 @@ import org.joda.time.DateTime;
 import org.joda.time.Weeks;
 import org.springframework.stereotype.Service;
 
-import java.io.FileOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Service
 public class ContractGenerationService {
 
+    private ContractService contractService;
+
+    public ContractGenerationService(ContractService contractService) {
+        this.contractService = contractService;
+    }
+
     public void generateContract(Contract contract) throws IOException {
-        //ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        //PdfWriter writer = new PdfWriter(stream);
-        OutputStream fos = new FileOutputStream("contract.pdf");
-        PdfWriter writer = new PdfWriter(fos);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        PdfWriter writer = new PdfWriter(stream);
+        //OutputStream fos = new FileOutputStream("contract.pdf");
+        //PdfWriter writer = new PdfWriter(fos);
         PdfDocument pdf = new PdfDocument(writer);
         Document document = new Document(pdf, PageSize.A4);
         document.add(new Paragraph("CONTRAT DE STAGE").setBold().setFontSize(20)
@@ -50,7 +55,7 @@ public class ContractGenerationService {
                 .add(new Text("L'étudiant(e), " + contract.getStudentApplication().getStudent().getFirstName() + " " + contract.getStudentApplication().getStudent().getLastName() + "\n\n"))
                 .add(new Text("Conviennent des conditions de stage suivantes : "));
         document.add(paragraph.setTextAlignment(TextAlignment.CENTER));
-        addIntershipInfoTable(contract, document);
+        addInternshipInfoTable(contract, document);
         document.add(new AreaBreak(AreaBreakType.NEXT_PAGE));
         document.add(new Paragraph(new Text("TACHES ET RESPONSABILITES DU STAGIAIRE\n").setBold()));
         float documentWidth = document.getPageEffectiveArea(PageSize.A4).getWidth();
@@ -58,7 +63,10 @@ public class ContractGenerationService {
         internshipPartiesResponsabilities(contract, document);
         signaturesSection(document, documentWidth);
         document.close();
-        //System.out.println( java.util.Base64.getEncoder().encodeToString(stream.toByteArray()));
+        String fileBase64 = com.itextpdf.io.codec.Base64.encodeBytes(stream.toByteArray());
+        contract.setFile(fileBase64);
+        contractService.createAndSaveNewContract(contract);
+        com.itextpdf.io.codec.Base64.decodeToFile(fileBase64, "test.pdf");
     }
 
     private void signaturesSection(Document document, float documentWidth) {
@@ -68,7 +76,7 @@ public class ContractGenerationService {
         document.add(new Paragraph(new Text(" Les parties s’engagent à respecter cette entente de stage\nEn foi de quoi les parties ont signé,").setBold().setTextAlignment(TextAlignment.CENTER))
                 .setFirstLineIndent(15f)
                 .add(new Text("\nL’étudiant(e) :\n").setBold())
-                .add(new Paragraph(new Text("[signature_etudiant]")).setMarginRight(120f).setMarginBottom(0))
+                .add(new Paragraph(new Text("[signature_etudiant]\n")).setMarginRight(120f).setMarginBottom(0))
                 .add(new Text("[date_signature_etudiant]"))
                 .add(new LineSeparator(new SolidLine(1)).setMarginTop(-4)
                 ));
@@ -87,11 +95,9 @@ public class ContractGenerationService {
                         .add(new Paragraph("[Date]").setMarginLeft(145f)));
     }
 
-
     private void internshipPartiesResponsabilities(Contract contract, Document document) throws IOException {
         PdfFont font = PdfFontFactory.createFont(StandardFonts.TIMES_ROMAN);
         document.add(new Paragraph(new Text("RESPONSABILITES\n").setBold()).setTextAlignment(TextAlignment.CENTER));
-
         document.add(new Paragraph(new Text("Le Collège s’engage à :\n").setBold())
                 .add(new Paragraph(contract.getEngagementCollege())).setFont(font)
                 .add(new Text("\nL'entreprise s’engage à :\n").setBold())
@@ -99,37 +105,36 @@ public class ContractGenerationService {
                 .add(new Text("\nL'étudiant s’engage à :\n").setBold())
                 .add(new Paragraph(contract.getEngagementStudent()))
         );
-
     }
 
     public String parseDate(Date date) {
         return new SimpleDateFormat("dd/MM/yy").format(date);
     }
 
-    public int dateInteravalToWeeks(Date endDate, Date startDate) {
+    public int dateIntervalToWeeks(Date endDate, Date startDate) {
         return Weeks.weeksBetween(new DateTime(endDate), new DateTime(startDate)).getWeeks();
     }
 
-    private void addIntershipInfoTable(Contract contract, Document document) {
+    private void addInternshipInfoTable(Contract contract, Document document) {
         InternshipOffer offer = contract.getStudentApplication().getOffer();
         Table internshipInfoTable = new Table(1).setWidth(500f);
         internshipInfoTable.setBorder(new SolidBorder(1f));
         internshipInfoTable.addCell(new Cell().setPadding(0).setBorder(Border.NO_BORDER)
                 .add(new Paragraph("ENDROIT DU STAGE").setBold().setMultipliedLeading(1.2f).setBackgroundColor(WebColors.getRGBColor("#DCDCDC"))));
         internshipInfoTable.addCell(new Cell().setPadding(0).setBorder(Border.NO_BORDER)
-                .add(new Paragraph("Adresse: " + contract.getStudentApplication().getOffer().getEmployer().getAddress()).setMultipliedLeading(1.2f)));
+                .add(new Paragraph("Adresse : " + contract.getStudentApplication().getOffer().getEmployer().getAddress()).setMultipliedLeading(1.2f)));
 
         internshipInfoTable.addCell(new Cell().setPadding(0).setBorder(Border.NO_BORDER)
                 .add(new Paragraph("DUREE DU STAGE").setBold().setMultipliedLeading(1.2f).setBackgroundColor(WebColors.getRGBColor("#DCDCDC"))));
         internshipInfoTable.addCell(new Cell().setPadding(0).setBorder(Border.NO_BORDER)
-                .add(new Paragraph("Date de début: " + parseDate(offer.getInternshipStartDate()) +
+                .add(new Paragraph("Date de début : " + parseDate(offer.getInternshipStartDate()) +
                         "\nDate de fin: " + parseDate(offer.getInternshipEndDate())
-                        + "\nNombre total de semaines: " + dateInteravalToWeeks(offer.getInternshipEndDate(), offer.getInternshipStartDate()) + "\n").setMultipliedLeading(1.2f)));
+                        + "\nNombre total de semaines : " + dateIntervalToWeeks(offer.getInternshipEndDate(), offer.getInternshipStartDate()) + "\n").setMultipliedLeading(1.2f)));
 
         internshipInfoTable.addCell(new Cell().setPadding(0).setBorder(Border.NO_BORDER)
                 .add(new Paragraph("HORAIRE DE TRAVAIL").setBold().setMultipliedLeading(1.2f).setBackgroundColor(WebColors.getRGBColor("#DCDCDC"))));
         internshipInfoTable.addCell(new Cell().setPadding(0).setBorder(Border.NO_BORDER)
-                .add(new Paragraph("Horaire de travail: " + contract.getHoraire() + "\nNombre total d’heures par semaine: " + contract.getTotalHoursPerWeek() + "h\n").setMultipliedLeading(1.2f)));
+                .add(new Paragraph("Horaire de travail : " + contract.getHoraire() + "\nNombre total d’heures par semaine: " + contract.getTotalHoursPerWeek() + "h\n").setMultipliedLeading(1.2f)));
 
         internshipInfoTable.addCell(new Cell().setPadding(0).setBorder(Border.NO_BORDER)
                 .add(new Paragraph("SALAIRE").setBold().setMultipliedLeading(1.2f).setBackgroundColor(WebColors.getRGBColor("#DCDCDC"))));
@@ -137,5 +142,4 @@ public class ContractGenerationService {
                 .add(new Paragraph("Salaire : " + offer.getSalary() + "$").setMultipliedLeading(1.2f)));
         document.add(internshipInfoTable);
     }
-
 }
