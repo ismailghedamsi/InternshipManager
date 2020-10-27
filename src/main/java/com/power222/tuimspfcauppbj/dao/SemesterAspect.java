ffcabd3f386666229bb782c8330a6688d993d189
Simon Longpré-Landry <simonllandry@hotmail.com>
2020-10-27 08:23:47 -0400

    EQ3-119: Retrait d'un pointcut inutile dans SemesterAspect

--- src/main/java/com/power222/tuimspfcauppbj/dao/SemesterAspect.java
+++ src/main/java/com/power222/tuimspfcauppbj/dao/SemesterAspect.java
@@ -1,11 +1,8 @@
 package com.power222.tuimspfcauppbj.dao;
 
-import com.power222.tuimspfcauppbj.model.SemesterDiscriminatedEntity;
 import com.power222.tuimspfcauppbj.util.SemesterContext;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.hibernate.Filter;
 import org.hibernate.Session;
 import org.springframework.stereotype.Component;
 
@@ -23,18 +20,7 @@ public class SemesterAspect {
     public void activateTenantFilter() {
         String semester = SemesterContext.isSet() ? SemesterContext.getCurrent() : SemesterContext.getPresentSemester();
         Session session = entityManager.unwrap(Session.class);
-        Filter filter = session.enableFilter("semesterFilter");
-        filter.setParameter("semester", semester);
-    }
-
-
-    @Before("execution(* org.springframework.data.repository.Repository+.save*(..))")
-    public void beforeSaves(JoinPoint jp) {
-        var o = jp.getArgs()[0];
-        if (o instanceof SemesterDiscriminatedEntity) {
-            var entity = (SemesterDiscriminatedEntity) o;
-            if (entity.getSemester() == null || entity.getSemester().isBlank())
-                entity.setSemester(SemesterContext.isSet() ? SemesterContext.getCurrent() : SemesterContext.getPresentSemester());
-        }
+        session.enableFilter("semesterFilter")
+                .setParameter("semester", semester);
     }
 }
