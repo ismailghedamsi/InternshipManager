6ae95fe672dcf4982e00e1e05a49de363927d14a
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2020-10-29 10:31:21 -0400

    EQ3-121 : Add ResponseEntities to Post /api/contractGeneration endpoint

--- src/main/java/com/power222/tuimspfcauppbj/controller/ContractGenerationController.java
+++ src/main/java/com/power222/tuimspfcauppbj/controller/ContractGenerationController.java
@@ -2,12 +2,13 @@ package com.power222.tuimspfcauppbj.controller;
 
 import com.power222.tuimspfcauppbj.model.ContractDto;
 import com.power222.tuimspfcauppbj.service.ContractGenerationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @RestController
@@ -20,13 +21,16 @@ public class ContractGenerationController {
     }
 
     @PostMapping
-    public void generateContract(@RequestBody ContractDto contract) {
+    public ResponseEntity<ContractDto> generateContract(@RequestBody ContractDto contract) {
+        if (contract == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
         try {
             service.generateContract(contract);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
+        return ResponseEntity.status(HttpStatus.CREATED).body(contract);
     }
 }
