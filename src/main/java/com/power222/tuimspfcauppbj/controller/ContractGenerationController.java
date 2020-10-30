9d4292f56644441418c0caef40f52ab171a6b69f
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2020-10-30 19:13:00 -0400

    EQ3-121 : Tests for ContractGenerationController

--- src/main/java/com/power222/tuimspfcauppbj/controller/ContractGenerationController.java
+++ src/main/java/com/power222/tuimspfcauppbj/controller/ContractGenerationController.java
@@ -1,7 +1,7 @@
 package com.power222.tuimspfcauppbj.controller;
 
-import com.power222.tuimspfcauppbj.model.ContractDto;
 import com.power222.tuimspfcauppbj.service.ContractGenerationService;
+import com.power222.tuimspfcauppbj.util.ContractDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,12 +20,6 @@ public class ContractGenerationController {
 
     @PostMapping
     public ResponseEntity<ContractDto> generateContract(@RequestBody ContractDto contract) {
-        if (contract == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-        if (!service.generateContract(contract)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
         return ResponseEntity.status(HttpStatus.CREATED).body(contract);
     }
 }
