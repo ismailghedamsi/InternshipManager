59aead4f28a41bbff1641edd47be68a1b064ff5e
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2020-11-02 20:31:10 -0500

    EQ3-124 : Remove attachment

--- src/main/java/com/power222/tuimspfcauppbj/service/MailSendingService.java
+++ src/main/java/com/power222/tuimspfcauppbj/service/MailSendingService.java
@@ -1,55 +1,51 @@
 package com.power222.tuimspfcauppbj.service;
 
 import com.power222.tuimspfcauppbj.model.Employer;
-import com.power222.tuimspfcauppbj.model.StudentApplication;
 import com.power222.tuimspfcauppbj.model.User;
-import com.power222.tuimspfcauppbj.util.MailContractDto;
-import org.springframework.core.io.ByteArrayResource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import java.util.Optional;
+
 
 @Service
+@Slf4j
 public class MailSendingService {
-    private JavaMailSender javaMailSender;
+    private final JavaMailSender javaMailSender;
 
-    private StudentApplicationService applicationService;
-
-    public MailSendingService(JavaMailSender javaMailSender, StudentApplicationService applicationService) {
+    public MailSendingService(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
-        this.applicationService = applicationService;
     }
 
-    public void sendEmail(User user, final String fileName, final String fileContent) {
+    public void sendEmail(User user) {
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-        MimeMessageHelper helper;
+        String sendTo = getUserEmail(user);
+        setMailContent(mimeMessage, sendTo);
+        javaMailSender.send(mimeMessage);
+    }
+
+    public MimeMessageHelper setMailContent(MimeMessage mimeMessage, String sendTo) {
+        MimeMessageHelper helper = null;
         try {
             helper = new MimeMessageHelper(mimeMessage, true, "utf-8");
-            String sendTo = "";
-            if (user instanceof Employer) {
-                sendTo = ((Employer) user).getEmail();
-            }
             String htmlMsg = "<h1> This is your contract </h1>";
-
-            // add attachment encode in base64
-
-            byte[] decodedPdfContent = com.itextpdf.io.codec.Base64.decode(fileContent);
-            helper.addAttachment(fileName, new ByteArrayResource(decodedPdfContent));
             helper.setTo(sendTo);
             helper.setSubject("Internship contract");
             helper.setText(htmlMsg, true);
-            javaMailSender.send(mimeMessage);
         } catch (MessagingException e) {
-            e.printStackTrace();
+            log.error("Imposible d'envoyer un email ", e.getMessage());
         }
+        return helper;
     }
 
-    public Optional<StudentApplication> getStudentApplication(MailContractDto mailContractDto) {
-        return applicationService.getApplicationById(mailContractDto.getStudentApplicationId());
+    private String getUserEmail(User user) {
+        String sentTo = "";
+        if (user instanceof Employer) {
+            sentTo = ((Employer) user).getEmail();
+        }
+        return sentTo;
     }
-
 }
