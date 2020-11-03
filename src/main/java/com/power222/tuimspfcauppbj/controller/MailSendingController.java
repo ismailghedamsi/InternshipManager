a9e4df10b90775b33288498358792adeb7169e97
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2020-11-02 20:29:05 -0500

    EQ3-124 : Remove useless application service

--- src/main/java/com/power222/tuimspfcauppbj/controller/MailSendingController.java
+++ src/main/java/com/power222/tuimspfcauppbj/controller/MailSendingController.java
@@ -3,6 +3,7 @@ package com.power222.tuimspfcauppbj.controller;
 import com.power222.tuimspfcauppbj.model.StudentApplication;
 import com.power222.tuimspfcauppbj.service.ContractService;
 import com.power222.tuimspfcauppbj.service.MailSendingService;
+import com.power222.tuimspfcauppbj.service.StudentApplicationService;
 import com.power222.tuimspfcauppbj.util.MailContractDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,18 +18,20 @@ import java.util.Optional;
 @RequestMapping("/api/sendMail")
 public class MailSendingController {
     private final MailSendingService mailService;
+    private final StudentApplicationService applicationService;
 
-    public MailSendingController(MailSendingService mailService, ContractService contractService) {
+    public MailSendingController(MailSendingService mailService, ContractService contractService, StudentApplicationService applicationService) {
         this.mailService = mailService;
+        this.applicationService = applicationService;
     }
 
     @PostMapping("/contract")
     public ResponseEntity SendContractByMail(@RequestBody MailContractDto contract) {
-        Optional<StudentApplication> optionalStudentApplication = mailService.getStudentApplication(contract);
+        Optional<StudentApplication> optionalStudentApplication = applicationService.getApplicationById(contract.getStudentApplicationId());
         if (optionalStudentApplication.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
-        mailService.sendEmail(optionalStudentApplication.get().getOffer().getEmployer(), "contract" + contract + ".pdf", contract.getFile());
+        mailService.sendEmail(optionalStudentApplication.get().getOffer().getEmployer());
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
