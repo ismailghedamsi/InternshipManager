b2e243077e5e729778363baf1cfef4a019d0430c
Simon Longpré-Landry <simonllandry@hotmail.com>
2020-11-03 16:57:44 -0500

    EQ3-01: Cherry-picking for semester commit that was lost

--- react-frontend/src/Components/Utils/Header/Menu.js
+++ react-frontend/src/Components/Utils/Header/Menu.js
@@ -1,9 +1,5 @@
 export const Menu = [
     {
-        title: 'Accueil',
-        url: '/dashboard'
-    },
-    {
         title: 'Approbation CV',
         url: '/dashboard/approbation/cv',
         role: 'admin'
