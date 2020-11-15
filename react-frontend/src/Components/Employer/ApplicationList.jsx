d7fb7a6dfccd261c8f140ebdc9d2af38a90afb9f
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2020-11-15 17:41:11 -0500

    EQ3-144 : Merge

--- react-frontend/src/Components/Employer/ApplicationList.jsx
+++ react-frontend/src/Components/Employer/ApplicationList.jsx
@@ -8,148 +8,198 @@ import PdfSelectionViewer from "../Utils/PdfSelectionViewer";
 import useStyles from "../Utils/useStyles";
 
 export default function ApplicationList() {
-    const classes = useStyles();
-    const location = useLocation();
-    const api = useApi();
-    const [offer, setOffer] = useState({});
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const classes = useStyles();
+  const location = useLocation();
+  const api = useApi();
+  const [offer, setOffer] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    useEffect(() => {
-        api.get("/offers/" + location.state.offerId)
-            .then(r => setOffer(r.data))
-    }, [location.state.offerId]) // eslint-disable-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    api.get("/offers/" + location.state.offerId).then((r) => setOffer(r.data));
+  }, [location.state.offerId]); // eslint-disable-line react-hooks/exhaustive-deps
 
-    function studentApplicationState(i) {
-        switch (offer.applications[i].state) {
-            case "APPLICATION_PENDING_FOR_EMPLOYER_INITIAL_REVIEW":
-            case "STUDENT_INVITED_FOR_INTERVIEW_BY_EMPLOYER":
-            case "WAITING_FOR_EMPLOYER_HIRING_FINAL_DECISION":
-            case "STUDENT_HIRED_BY_EMPLOYER":
-                if (AuthenticationService.getCurrentUserRole() === "admin" && offer.applications[i].state === "STUDENT_HIRED_BY_EMPLOYER") {
-                    return <Typography variant={"body1"} style={{color: "green"}}> Application acceptée </Typography>
-                }
-                return <Typography>
-                    {console.log(offer.applications[i].state)}
-                    Application acceptée:
-                    <Checkbox
-                        value="state"
-                        checked={offer.applications[i].state === "STUDENT_HIRED_BY_EMPLOYER"}
-                        onChange={
-                            () => {
-                                var copy = {...offer}
-                                copy.applications[i].state = copy.applications[i].state === "STUDENT_HIRED_BY_EMPLOYER" ?
-                                    "WAITING_FOR_EMPLOYER_HIRING_FINAL_DECISION" : "STUDENT_HIRED_BY_EMPLOYER"
-                                api.put(`applications/state/${offer.applications[i].id}`, offer.applications[i])
-                                    .then(r => {
-                                        if (r) {
-                                            copy.applications[i].state = r.data.state;
-                                        }
-                                        setOffer(copy)
-                                    });
-                            }}
-                        inputProps={{'aria-label': 'state'}}
-                    />
-                </Typography>
-            case "APPLICATION_REJECTED_BY_EMPLOYER":
-            case "STUDENT_REJECTED_BY_EMPLOYER":
-                return <Typography variant={"body1"} style={{color: "red"}}>
-                    L'employeur a refusé la demande
-                </Typography>
-            case "WAITING_FOR_STUDENT_HIRING_FINAL_DECISION":
-                return <Typography variant={"body1"} style={{color: "blue"}}>
-                    En attente de la décision de l'étudiant
-                </Typography>
-            case "JOB_OFFER_ACCEPTED_BY_STUDENT":
-                return <Typography variant={"body1"} style={{color: "green"}}>
-                    L'étudiant a été embauché
-                    {offer.applications[i].contract === null && AuthenticationService.getCurrentUserRole() === "admin" &&
-                    <Link variant={"body1"}
-                          to={{
-                              pathname: "/dashboard/contractForm",
-                              state: {...offer.applications[i]}
-                          }}
-                          style={{display: "block"}}
-                    >
-                        Genérer le contrat
-                    </Link>
-                    }
-                </Typography>
-            case "JOB_OFFER_DENIED_BY_STUDENT":
-                return <Typography variant={"body1"} style={{color: "red"}}>
-                    L'étudiant a refusé l'offre de stage
-                </Typography>
-            default:
-                return '';
+  function studentApplicationState(i) {
+    switch (offer.applications[i].state) {
+      case "APPLICATION_PENDING_FOR_EMPLOYER_INITIAL_REVIEW":
+      case "STUDENT_INVITED_FOR_INTERVIEW_BY_EMPLOYER":
+      case "WAITING_FOR_EMPLOYER_HIRING_FINAL_DECISION":
+      case "STUDENT_HIRED_BY_EMPLOYER":
+        if (
+                AuthenticationService.getCurrentUserRole() === "admin" &&
+                offer.applications[i].state === "STUDENT_HIRED_BY_EMPLOYER"
+        ) {
+          return (
+                  <Typography variant={"body1"} style={{color: "green"}}>
+
+                    Application acceptée
+                  </Typography>
+          );
         }
+        return (
+                <Typography>
+                  Application acceptée:
+                  <Checkbox
+                          value="state"
+                          checked={
+                            offer.applications[i].state === "STUDENT_HIRED_BY_EMPLOYER"
+                          }
+                          onChange={() => {
+                            var copy = {...offer};
+                            copy.applications[i].state =
+                                    copy.applications[i].state === "STUDENT_HIRED_BY_EMPLOYER"
+                                            ? "WAITING_FOR_EMPLOYER_HIRING_FINAL_DECISION"
+                                            : "STUDENT_HIRED_BY_EMPLOYER";
+                            api
+                                    .put(
+                                            `applications/state/${offer.applications[i].id}`,
+                                            offer.applications[i]
+                                    )
+                                    .then((r) => {
+                                      if (r) {
+                                        copy.applications[i].state = r.data.state;
+                                      }
+                                      setOffer(copy);
+                                    });
+                          }}
+                          inputProps={{"aria-label": "state"}}
+                  />
+                </Typography>
+        );
+      case "APPLICATION_REJECTED_BY_EMPLOYER":
+      case "STUDENT_REJECTED_BY_EMPLOYER":
+        return (
+                <Typography variant={"body1"} style={{color: "red"}}>
+                  L'employeur a refusé la demande
+                </Typography>
+        );
+      case "WAITING_FOR_STUDENT_HIRING_FINAL_DECISION":
+        return (
+                <Typography variant={"body1"} style={{color: "blue"}}>
+                  En attente de la décision de l'étudiant
+                </Typography>
+        );
+      case "JOB_OFFER_ACCEPTED_BY_STUDENT":
+        return (
+                <Typography variant={"body1"} style={{color: "green"}}>
+                  L'étudiant a été embauché
+                  {offer.applications[i].contract === null &&
+                  AuthenticationService.getCurrentUserRole() === "admin" && (
+                          <Link
+                                  variant={"body1"}
+                                  to={{
+                                    pathname: "/dashboard/contractForm",
+                                    state: {...offer.applications[i]},
+                                  }}
+                                  style={{display: "block"}}
+                          >
+                            Genérer le contrat
+                          </Link>
+                  )}
+                </Typography>
+        );
+      case "JOB_OFFER_DENIED_BY_STUDENT":
+        return (
+                <Typography variant={"body1"} style={{color: "red"}}>
+                  L'étudiant a refusé l'offre de stage
+                </Typography>
+        );
+      default:
+        return "";
     }
+  }
 
-    function evaluationDirection() {
-        if (AuthenticationService.getCurrentUserRole() === "admin")
-            return "/dashboard/businessEvaluation";
-        else
-            return "/dashboard/evaluateStudent";
-    }
+  function evaluationDirection() {
+    if (AuthenticationService.getCurrentUserRole() === "admin")
+      return "/dashboard/businessEvaluation";
+    else return "/dashboard/evaluateStudent";
+  }
 
-    function roleCondition(i) {
-        if (AuthenticationService.getCurrentUserRole() === "admin")
-            return offer.applications[i].contract.businessEvaluation === null;
-        else
-            return offer.applications[i].contract.internEvaluation === null;
-    }
+  function roleCondition(i) {
+    if (AuthenticationService.getCurrentUserRole() === "admin")
+      return offer.applications[i].contract.businessEvaluation === null;
+    else return offer.applications[i].contract.internEvaluation === null;
+  }
 
-    return <div style={{height: "100%"}}>
-        <PdfSelectionViewer documents={(offer.applications ? offer.applications : []).map(o => o.resume.file)}
-                            title={<span>Application<br/>{offer.title}</span>}>
-            {(i, setCurrent) =>
-                <div key={i}>
-                    <button
-                        type={"button"}
-                        className={[classes.linkButton, classes.fileButton].join(' ')}
-                        autoFocus={i === 0}
-                        onClick={() => {
-                            setCurrent(i)
-                            setCurrentIndex(i)
-                        }}>
-                        <Typography color={"textPrimary"} variant={"h5"} style={{display: "block"}}>
-                            {offer.applications[i].student.firstName} {offer.applications[i].student.lastName}
-                        </Typography>
-                    </button>
-                    {currentIndex === i &&
-                    <div>
-                        <Typography color={"textPrimary"} variant={"body1"}>
-                            {offer.applications[i].student.phoneNumber} {offer.applications[i].student.email}
-                        </Typography>
-                        <Typography color={"textPrimary"} variant={"body1"}>
-                            {offer.applications[i].student.address}
-                        </Typography>
-                        {studentApplicationState(i)}
-                        {AuthenticationService.getCurrentUserRole() === "employer" &&
-                        offer.applications[i].state !== "STUDENT_INVITED_FOR_INTERVIEW_BY_EMPLOYER" &&
-                        <Link variant={"body1"}
-                              to={{
-                                  pathname: "/dashboard/interviewConvocation",
-                                  state: {...offer.applications[i]}
-                              }}
-                              style={{display: "block"}}
-                        >
-                            Convoquer l'étudiant pour un entrevue
-                        </Link>
-                        }
-                        {offer.applications[i].contract !== null && offer.applications[i].contract.signatureState === "SIGNED" && roleCondition(i) &&
-                        <Link variant={"body1"}
-                              to={{
-                                  pathname: evaluationDirection(),
-                                  state: {...offer.applications[i]}
-                              }}
-                              style={{display: "block"}}
-                        >
-                            {AuthenticationService.getCurrentUserRole() === "admin" ? "Évaluer l'entreprise" : "Évaluer l'étudiant"}
-                        </Link>
-                        }
-                    </div>
+  return (
+          <div style={{height: "100%"}}>
+            <PdfSelectionViewer
+                    documents={(offer.applications ? offer.applications : []).map(
+                            (o) => o.resume.file
+                    )}
+                    title={
+                      <span>
+            Application
+            <br/>
+                        {offer.title}
+          </span>
                     }
-                    <hr/>
-                </div>}
-        </PdfSelectionViewer>
-    </div>
+            >
+              {(i, setCurrent) => (
+                      <div key={i}>
+                        <button
+                                type={"button"}
+                                className={[classes.linkButton, classes.fileButton].join(" ")}
+                                autoFocus={i === 0}
+                                onClick={() => {
+                                  setCurrent(i);
+                                  setCurrentIndex(i);
+                                }}
+                        >
+                          <Typography
+                                  color={"textPrimary"}
+                                  variant={"h5"}
+                                  style={{display: "block"}}
+                          >
+                            {offer.applications[i].student.firstName}
+                            {offer.applications[i].student.lastName}
+                          </Typography>
+                        </button>
+                        {currentIndex === i && (
+                                <div>
+                                  <Typography color={"textPrimary"} variant={"body1"}>
+                                    {offer.applications[i].student.phoneNumber}
+                                    {offer.applications[i].student.email}
+                                  </Typography>
+                                  <Typography color={"textPrimary"} variant={"body1"}>
+                                    {offer.applications[i].student.address}
+                                  </Typography>
+                                  {studentApplicationState(i)}
+                                  {AuthenticationService.getCurrentUserRole() === "employer" &&
+                                  offer.applications[i].state !==
+                                  "STUDENT_INVITED_FOR_INTERVIEW_BY_EMPLOYER" && (
+                                          <Link
+                                                  variant={"body1"}
+                                                  to={{
+                                                    pathname: "/dashboard/interviewConvocation",
+                                                    state: {...offer.applications[i]},
+                                                  }}
+                                                  style={{display: "block"}}
+                                          >
+                                            Convoquer l'étudiant pour un entrevue
+                                          </Link>
+                                  )}
+                                  {offer.applications[i].contract !== null &&
+                                  offer.applications[i].contract.signatureState === "SIGNED" &&
+                                  roleCondition(i) && (
+                                          <Link
+                                                  variant={"body1"}
+                                                  to={{
+                                                    pathname: evaluationDirection(),
+                                                    state: {...offer.applications[i]},
+                                                  }}
+                                                  style={{display: "block"}}
+                                          >
+                                            {AuthenticationService.getCurrentUserRole() === "admin"
+                                                    ? "Évaluer l'entreprise"
+                                                    : "Évaluer l'étudiant"}
+                                          </Link>
+                                  )}
+                                </div>
+                        )}
+                        <hr/>
+                      </div>
+              )}
+            </PdfSelectionViewer>
+          </div>
+  );
 }
