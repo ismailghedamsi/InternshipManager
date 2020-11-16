0e3f56ecac4b048b9511bb74544eab664734e0d4
Andrei <andrei.belkin.0@protonmail.com>
2020-11-15 23:24:36 -0500

    EQ3-36 : Update managers page after posting or putting

--- react-frontend/src/Components/Manager/Managers.jsx
+++ react-frontend/src/Components/Manager/Managers.jsx
@@ -17,8 +17,10 @@ import { TextField } from "formik-material-ui";
 import PropTypes from 'prop-types';
 import { default as React, useEffect, useState } from 'react';
 import * as yup from "yup";
+import AuthenticationService from "../../Services/AuthenticationService";
 import { useApi, useModal } from "../Utils/Hooks";
 import useStyles from "../Utils/useStyles";
+import {useHistory} from "react-router-dom";
 
 const requiredFieldMsg = "Ce champs est requis";
 const tooShortError = value => "Doit avoir au moins " + value.min + " caractères";
@@ -63,9 +65,10 @@ function DataTable() {
     const [itemCount, setItemCount] = useState(0)
     const [currentPage, setCurrentPage] = useState(0)
     const [rowsPerPage, setRowsPerPage] = useState(10)
-    const [rows, setRows] = useState([])
     const [currentManager, setCurrentManager] = useState({})
     const [isEditModalOpen, openEditModal, closeEditModal] = useModal()
+    const [isCreateModalOpen, openCreateModal, closeCreateModal] = useModal()
+    const [rows, setRows] = useState([])
 
     useEffect(() => {
         api.get("/admins?page=" + currentPage + "&itemsPerPage=" + rowsPerPage)
@@ -98,150 +101,76 @@ function DataTable() {
                 </TableFooter>
             </Table>
         </TableContainer>
-        <EditManager manager={currentManager} isOpen={isEditModalOpen} hide={closeEditModal} setRows={setRows}/>
+        <Button onClick={openCreateModal}
+            type="button"
+            variant="contained"
+            color="primary"
+            size="large"
+        >
+            Enregistrer un nouveau gestionnaire
+        </Button>
+        <EditManager manager={currentManager} isOpen={isEditModalOpen} hide={closeEditModal} setRows={setRows} setItemCount={setItemCount}/>
+        <CreateManager isOpen={isCreateModalOpen} hide={closeCreateModal} setRows={setRows} setItemCount={setItemCount}/>
     </ThemeProvider>
 }
 
-function EditManager({manager, isOpen, hide, setRows}) {
+function EditManager({manager, isOpen, hide, setRows, setItemCount}) {
     const api = useApi()
     const classes = useStyles()
+    const history = useHistory()
 
-    function toggleManagerDisabledState(manager) {
+    function toggleManagerDisabledState() {
         return api.put("admins/toggle/" + manager.id, {})
             .then(() => {
                 hide()
+                return api.get("admins")
+                    .then(response => {
+                        setRows(response.data.content)
+                        setItemCount(response.data.totalElements)
+                    })
             })
     }
 
     return isOpen && <Dialog open={isOpen} onClose={hide}>
-            <DialogTitle>{"Modifier gestionnaire de stage"}</DialogTitle>
+            <DialogTitle>{"Modifier gestionnaire de stage : " + manager.name}</DialogTitle>
             <DialogContent>
-                <DialogContentText>
-                    <Formik onSubmit={toggleManagerDisabledState} initialValues={{}}>
-                        {({isSubmitting}) =>
-                            <Form>
-                                    État du compte : {manager.disabled}
-                                    <Button type="submit"
-                                        variant="contained"
-                                        color="primary"
-                                        size="large"
-                                        disabled={isSubmitting}
-                                    >
-                                        {manager.disabled ? "Activer" : "Désactiver"}
-                                        {isSubmitting && <CircularProgress size={18}/>}
-                                    </Button>
-                                </Form>
-                        }
-                    </Formik>
-                    <br/>
-                    Changer le mot de passe :
-                    <Formik
-                        onSubmit={async values =>
-                            api.put("admins/password", values)
-                                .then(hide)
-                        }
-
-                        validationSchema={yup.object()
-                            .shape({
-                                username: yup.string().trim().required(requiredFieldMsg),
-                                newPassword: yup.string().trim().min(8, tooShortError).required(requiredFieldMsg),
-                                newConfirm: yup.string()
-                                    .oneOf([yup.ref('newPassword'), null], "Les mots de passes doivent êtres identiques").required(requiredFieldMsg)
-                            })}
-                        validateOnBlur={false}
-                        validateOnChange={false}
-                        enableReinitialize={true}
-                        initialValues={{
-                            username: manager.username,
-                            newPassword: '',
-                            newConfirm: ''
-                        }}
-                    >
-                        {({isSubmitting}) => <Form className={classes.form}>
-                            <Grid container spacing={2}>
-                                <Grid item xs={6}>
-                                    <Field
-                                        component={TextField}
-                                        name="username"
-                                        id="username"
-                                        variant="outlined"
-                                        label="Nom d'utilisateur"
-                                        disabled
-                                        required
-                                        fullWidth
-                                    />
-                                </Grid>
-                                <Grid item xs={6}>
-                                    <Field
-                                        component={TextField}
-                                        name="newPassword"
-                                        id="newPassword"
-                                        variant="outlined"
-                                        label="Nouveau mot de passe"
-                                        type={"password"}
-                                        required
-                                        fullWidth
-                                    />
-                                </Grid>
-                                <Grid item xs={6}>
-                                    <Field
-                                        component={TextField}
-                                        name="newConfirm"
-                                        id="newConfirm"
-                                        variant="outlined"
-                                        label="Confirmez"
-                                        type={"password"}
-                                        required
-                                        fullWidth
-                                    />
-                                </Grid>
-                            </Grid>
-                            <br/>
-                            {isSubmitting && <LinearProgress/>}
-                            <Button
-                                type={"submit"}
-                                fullWidth
+                <Formik onSubmit={toggleManagerDisabledState} initialValues={{}}>
+                    {({isSubmitting}) =>
+                        <Form>
+                            État du compte : {manager.disabled ? "Inactif" : "Actif"}&emsp;
+                            <Button type="submit"
                                 variant="contained"
                                 color="primary"
-                                size={"large"}
-                                className={classes.submit}
+                                size="large"
                                 disabled={isSubmitting}
                             >
-                                Changer le mot de passe
+                                {manager.disabled ? "Activer" : "Désactiver"}
+                                {isSubmitting && <CircularProgress size={18}/>}
                             </Button>
-                        </Form>}
-                    </Formik>
-                </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={hide} color="primary">
-                    Annuler
-                </Button>
-            </DialogActions>
-        </Dialog>;
-}
-
-function CreateManager({isOpen, hide, setRows}) {
-    const api = useApi()
-    const classes = useStyles()
-
-    return isOpen && <Dialog open={isOpen} onClose={hide}>
-        <DialogTitle>{"Créer gestionnaire de stage"}</DialogTitle>
-        <DialogContent>
-            <DialogContentText>
+                        </Form>
+                    }
+                </Formik>
+                <br/>
+                Changer le mot de passe :
                 <Formik
                     onSubmit={async values =>
-                        api.post("admins", values)
-                        .then(hide)
+                        api.put("admins/password", values)
+                            .then(() => {
+                                hide()
+                                if (manager.id == AuthenticationService.getCurrentUser().id) {
+                                    AuthenticationService.logout()
+                                    history.push("/")
+                                }
+                            })
                     }
 
                     validationSchema={yup.object()
-                    .shape({
-                        username: yup.string().trim().required(requiredFieldMsg),
-                        newPassword: yup.string().trim().min(8, tooShortError).required(requiredFieldMsg),
-                        newConfirm: yup.string()
-                        .oneOf([yup.ref('newPassword'), null], "Les mots de passes doivent êtres identiques").required(requiredFieldMsg)
-                    })}
+                        .shape({
+                            username: yup.string().trim().required(requiredFieldMsg),
+                            newPassword: yup.string().trim().min(8, tooShortError).required(requiredFieldMsg),
+                            newConfirm: yup.string()
+                                .oneOf([yup.ref('newPassword'), null], "Les mots de passes doivent êtres identiques").required(requiredFieldMsg)
+                        })}
                     validateOnBlur={false}
                     validateOnChange={false}
                     enableReinitialize={true}
@@ -253,18 +182,6 @@ function CreateManager({isOpen, hide, setRows}) {
                 >
                     {({isSubmitting}) => <Form className={classes.form}>
                         <Grid container spacing={2}>
-                            <Grid item xs={6}>
-                                <Field
-                                    component={TextField}
-                                    name="username"
-                                    id="username"
-                                    variant="outlined"
-                                    label="Nom d'utilisateur"
-                                    disabled
-                                    required
-                                    fullWidth
-                                />
-                            </Grid>
                             <Grid item xs={6}>
                                 <Field
                                     component={TextField}
@@ -305,6 +222,130 @@ function CreateManager({isOpen, hide, setRows}) {
                         </Button>
                     </Form>}
                 </Formik>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={hide} color="primary">
+                    Annuler
+                </Button>
+            </DialogActions>
+        </Dialog>;
+}
+
+function CreateManager({isOpen, hide, setRows, setItemCount}) {
+    const api = useApi()
+    const classes = useStyles()
+
+    return isOpen && <Dialog open={isOpen} onClose={hide}>
+        <DialogTitle>{"Créer gestionnaire de stage"}</DialogTitle>
+        <DialogContent>
+            <DialogContentText>
+                <Formik
+                    onSubmit={async values =>
+                        api.post("admins", values)
+                        .then(() => {
+                            hide()
+                            return api.get("admins")
+                                .then(response => {
+                                    setRows(response.data.content)
+                                    setItemCount(response.data.totalElements)
+                                })
+                        })
+                    }
+
+                    validationSchema={yup.object()
+                    .shape({
+                        username: yup.string().trim().required(requiredFieldMsg),
+                        name: yup.string().trim().required(requiredFieldMsg),
+                        email: yup.string().trim().email("L'email n'a pas un format valide").required(requiredFieldMsg),
+                        password: yup.string().trim().min(8, tooShortError).required(requiredFieldMsg),
+                        confirm: yup.string().oneOf([yup.ref('password'), null], "Les mots de passes doivent êtres identiques").required(requiredFieldMsg)
+                    })}
+                    validateOnBlur={false}
+                    validateOnChange={false}
+                    enableReinitialize={true}
+                    initialValues={{
+                        username: '',
+                        name: '',
+                        email: '',
+                        password: '',
+                        confirm: ''
+                    }}
+                >
+                    {({isSubmitting}) => <Form className={classes.form}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Field
+                                    component={TextField}
+                                    name="name"
+                                    id="name"
+                                    variant="outlined"
+                                    label="Nom complet"
+                                    required
+                                    fullWidth
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Field
+                                    component={TextField}
+                                    name="email"
+                                    id="email"
+                                    variant="outlined"
+                                    label="Adresse courriel"
+                                    required
+                                    fullWidth
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Field
+                                    component={TextField}
+                                    name="username"
+                                    id="username"
+                                    variant="outlined"
+                                    label="Nom d'utilisateur"
+                                    required
+                                    fullWidth
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Field
+                                    component={TextField}
+                                    name="password"
+                                    id="password"
+                                    variant="outlined"
+                                    label="Nouveau mot de passe"
+                                    type={"password"}
+                                    required
+                                    fullWidth
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Field
+                                    component={TextField}
+                                    name="confirm"
+                                    id="confirm"
+                                    variant="outlined"
+                                    label="Confirmez"
+                                    type={"password"}
+                                    required
+                                    fullWidth
+                                />
+                            </Grid>
+                        </Grid>
+                        <br/>
+                        {isSubmitting && <LinearProgress/>}
+                        <Button
+                            type={"submit"}
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            size={"large"}
+                            className={classes.submit}
+                            disabled={isSubmitting}
+                        >
+                            Enregistrer le gestionnaire de stage
+                        </Button>
+                    </Form>}
+                </Formik>
             </DialogContentText>
         </DialogContent>
         <DialogActions>
@@ -319,7 +360,15 @@ EditManager.propTypes = {
     manager: PropTypes.object.isRequired,
     isOpen: PropTypes.any.isRequired,
     hide: PropTypes.func.isRequired,
-    setRows: PropTypes.func.isRequired
+    setRows: PropTypes.func.isRequired,
+    setItemCount: PropTypes.func.isRequired
+}
+
+CreateManager.propTypes = {
+    isOpen: PropTypes.any.isRequired,
+    hide: PropTypes.func.isRequired,
+    setRows: PropTypes.func.isRequired,
+    setItemCount: PropTypes.func.isRequired
 }
 
 export default function Managers() {
