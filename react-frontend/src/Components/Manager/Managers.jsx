fde82ba786da265b15938918329e866548a20dca
songx198 <moko1125986@gmail.com>
2020-11-18 02:22:39 -0500

    bugfix

--- react-frontend/src/Components/Manager/Managers.jsx
+++ react-frontend/src/Components/Manager/Managers.jsx
@@ -1,4 +1,13 @@
-import { Button, Table, TableBody, TableContainer, TableFooter, TableHead, TablePagination, TableRow } from "@material-ui/core";
+import {
+    Button,
+    Table,
+    TableBody,
+    TableContainer,
+    TableFooter,
+    TableHead,
+    TablePagination,
+    TableRow
+} from "@material-ui/core";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
@@ -12,13 +21,13 @@ import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 import TableCell from "@material-ui/core/TableCell";
 import Typography from "@material-ui/core/Typography";
 import ThemeProvider from "@material-ui/styles/ThemeProvider";
-import { Field, Form, Formik } from "formik";
-import { TextField } from "formik-material-ui";
+import {Field, Form, Formik} from "formik";
+import {TextField} from "formik-material-ui";
 import PropTypes from 'prop-types';
-import { default as React, useEffect, useState } from 'react';
+import {default as React, useEffect, useState} from 'react';
 import * as yup from "yup";
 import AuthenticationService from "../../Services/AuthenticationService";
-import { useApi, useModal } from "../Utils/Hooks";
+import {useApi, useModal} from "../Utils/Hooks";
 import useStyles from "../Utils/useStyles";
 import {useHistory} from "react-router-dom";
 
@@ -102,15 +111,18 @@ function DataTable() {
             </Table>
         </TableContainer>
         <Button onClick={openCreateModal}
-            type="button"
-            variant="contained"
-            color="primary"
-            size="large"
+                style={{marginLeft: "10"}}
+                type="button"
+                variant="contained"
+                color="primary"
+                size="large"
         >
             Enregistrer un nouveau gestionnaire
         </Button>
-        <EditManager manager={currentManager} isOpen={isEditModalOpen} hide={closeEditModal} setRows={setRows} setItemCount={setItemCount}/>
-        <CreateManager isOpen={isCreateModalOpen} hide={closeCreateModal} setRows={setRows} setItemCount={setItemCount}/>
+        <EditManager manager={currentManager} isOpen={isEditModalOpen} hide={closeEditModal} setRows={setRows}
+                     setItemCount={setItemCount}/>
+        <CreateManager isOpen={isCreateModalOpen} hide={closeCreateModal} setRows={setRows}
+                       setItemCount={setItemCount}/>
     </ThemeProvider>
 }
 
@@ -132,103 +144,103 @@ function EditManager({manager, isOpen, hide, setRows, setItemCount}) {
     }
 
     return isOpen && <Dialog open={isOpen} onClose={hide}>
-            <DialogTitle>{"Modifier gestionnaire de stage : " + manager.name}</DialogTitle>
-            <DialogContent>
-                <Formik onSubmit={toggleManagerDisabledState} initialValues={{}}>
-                    {({isSubmitting}) =>
-                        <Form>
-                            État du compte : {manager.disabled ? "Inactif" : "Actif"}&emsp;
-                            <Button type="submit"
+        <DialogTitle>{"Modifier gestionnaire de stage : " + manager.name}</DialogTitle>
+        <DialogContent>
+            <Formik onSubmit={toggleManagerDisabledState} initialValues={{}}>
+                {({isSubmitting}) =>
+                    <Form>
+                        État du compte : {manager.disabled ? "Inactif" : "Actif"}&emsp;
+                        <Button type="submit"
                                 variant="contained"
                                 color="primary"
                                 size="large"
                                 disabled={isSubmitting}
-                            >
-                                {manager.disabled ? "Activer" : "Désactiver"}
-                                {isSubmitting && <CircularProgress size={18}/>}
-                            </Button>
-                        </Form>
-                    }
-                </Formik>
-                <br/>
-                Changer le mot de passe :
-                <Formik
-                    onSubmit={async values =>
-                        api.put("admins/password", values)
-                            .then(() => {
-                                hide()
-                                if (manager.id === AuthenticationService.getCurrentUser().id) {
-                                    AuthenticationService.logout()
-                                    history.push("/")
-                                }
-                            })
-                    }
-
-                    validationSchema={yup.object()
-                        .shape({
-                            username: yup.string().trim().required(requiredFieldMsg),
-                            newPassword: yup.string().trim().min(8, tooShortError).required(requiredFieldMsg),
-                            newConfirm: yup.string()
-                                .oneOf([yup.ref('newPassword'), null], "Les mots de passes doivent êtres identiques").required(requiredFieldMsg)
-                        })}
-                    validateOnBlur={false}
-                    validateOnChange={false}
-                    enableReinitialize={true}
-                    initialValues={{
-                        username: manager.username,
-                        newPassword: '',
-                        newConfirm: ''
-                    }}
-                >
-                    {({isSubmitting}) => <Form className={classes.form}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={6}>
-                                <Field
-                                    component={TextField}
-                                    name="newPassword"
-                                    id="newPassword"
-                                    variant="outlined"
-                                    label="Nouveau mot de passe"
-                                    type={"password"}
-                                    required
-                                    fullWidth
-                                />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <Field
-                                    component={TextField}
-                                    name="newConfirm"
-                                    id="newConfirm"
-                                    variant="outlined"
-                                    label="Confirmez"
-                                    type={"password"}
-                                    required
-                                    fullWidth
-                                />
-                            </Grid>
-                        </Grid>
-                        <br/>
-                        {isSubmitting && <LinearProgress/>}
-                        <Button
-                            type={"submit"}
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            size={"large"}
-                            className={classes.submit}
-                            disabled={isSubmitting}
                         >
-                            Changer le mot de passe
+                            {manager.disabled ? "Activer" : "Désactiver"}
+                            {isSubmitting && <CircularProgress size={18}/>}
                         </Button>
-                    </Form>}
-                </Formik>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={hide} color="primary">
-                    Annuler
-                </Button>
-            </DialogActions>
-        </Dialog>;
+                    </Form>
+                }
+            </Formik>
+            <br/>
+            Changer le mot de passe :
+            <Formik
+                onSubmit={async values =>
+                    api.put("admins/password", values)
+                        .then(() => {
+                            hide()
+                            if (manager.id === AuthenticationService.getCurrentUser().id) {
+                                AuthenticationService.logout()
+                                history.push("/")
+                            }
+                        })
+                }
+
+                validationSchema={yup.object()
+                    .shape({
+                        username: yup.string().trim().required(requiredFieldMsg),
+                        newPassword: yup.string().trim().min(8, tooShortError).required(requiredFieldMsg),
+                        newConfirm: yup.string()
+                            .oneOf([yup.ref('newPassword'), null], "Les mots de passes doivent êtres identiques").required(requiredFieldMsg)
+                    })}
+                validateOnBlur={false}
+                validateOnChange={false}
+                enableReinitialize={true}
+                initialValues={{
+                    username: manager.username,
+                    newPassword: '',
+                    newConfirm: ''
+                }}
+            >
+                {({isSubmitting}) => <Form className={classes.form}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={6}>
+                            <Field
+                                component={TextField}
+                                name="newPassword"
+                                id="newPassword"
+                                variant="outlined"
+                                label="Nouveau mot de passe"
+                                type={"password"}
+                                required
+                                fullWidth
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Field
+                                component={TextField}
+                                name="newConfirm"
+                                id="newConfirm"
+                                variant="outlined"
+                                label="Confirmez"
+                                type={"password"}
+                                required
+                                fullWidth
+                            />
+                        </Grid>
+                    </Grid>
+                    <br/>
+                    {isSubmitting && <LinearProgress/>}
+                    <Button
+                        type={"submit"}
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        size={"large"}
+                        className={classes.submit}
+                        disabled={isSubmitting}
+                    >
+                        Changer le mot de passe
+                    </Button>
+                </Form>}
+            </Formik>
+        </DialogContent>
+        <DialogActions>
+            <Button onClick={hide} color="primary">
+                Annuler
+            </Button>
+        </DialogActions>
+    </Dialog>;
 }
 
 function CreateManager({isOpen, hide, setRows, setItemCount}) {
@@ -242,24 +254,24 @@ function CreateManager({isOpen, hide, setRows, setItemCount}) {
                 <Formik
                     onSubmit={async values =>
                         api.post("admins", values)
-                        .then(() => {
-                            hide()
-                            return api.get("admins")
-                                .then(response => {
-                                    setRows(response.data.content)
-                                    setItemCount(response.data.totalElements)
-                                })
-                        })
+                            .then(() => {
+                                hide()
+                                return api.get("admins")
+                                    .then(response => {
+                                        setRows(response.data.content)
+                                        setItemCount(response.data.totalElements)
+                                    })
+                            })
                     }
 
                     validationSchema={yup.object()
-                    .shape({
-                        username: yup.string().trim().required(requiredFieldMsg),
-                        name: yup.string().trim().required(requiredFieldMsg),
-                        email: yup.string().trim().email("L'email n'a pas un format valide").required(requiredFieldMsg),
-                        password: yup.string().trim().min(8, tooShortError).required(requiredFieldMsg),
-                        confirm: yup.string().oneOf([yup.ref('password'), null], "Les mots de passes doivent êtres identiques").required(requiredFieldMsg)
-                    })}
+                        .shape({
+                            username: yup.string().trim().required(requiredFieldMsg),
+                            name: yup.string().trim().required(requiredFieldMsg),
+                            email: yup.string().trim().email("L'email n'a pas un format valide").required(requiredFieldMsg),
+                            password: yup.string().trim().min(8, tooShortError).required(requiredFieldMsg),
+                            confirm: yup.string().oneOf([yup.ref('password'), null], "Les mots de passes doivent êtres identiques").required(requiredFieldMsg)
+                        })}
                     validateOnBlur={false}
                     validateOnChange={false}
                     enableReinitialize={true}
