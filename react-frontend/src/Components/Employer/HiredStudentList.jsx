5e2135409640206b814d0c72a5839ff18e3ffe5b
Ernesto231 <max2power222gmail.com>
2020-11-20 21:07:16 -0500

    EQ3-166: affichage des etudians embaucher fini

--- react-frontend/src/Components/Employer/HiredStudentList.jsx
+++ react-frontend/src/Components/Employer/HiredStudentList.jsx
@@ -1,23 +1,16 @@
-import React, {useEffect, useState} from "react"
-import {useApi} from "../Utils/Hooks"
-import useStyles from "../Utils/useStyles"
 import {Typography} from "@material-ui/core";
+import React, {useEffect, useState} from "react";
 import AuthenticationService from "../../Services/AuthenticationService";
+import {useApi} from "../Utils/Hooks";
+import OfferDetails from "../Utils/OfferDetails";
 import PdfSelectionViewer from "../Utils/PdfSelectionViewer";
+import useStyles from "../Utils/useStyles";
 
 export default function HiredStudentList() {
     const classes = useStyles()
     const api = useApi()
     const [contracts, setContracts] = useState([])
-    const [applications, setApplications] = useState([])
-    const [currentOffer, setCurrentOffer] = useState([])
     const [currentIndex, setCurrentIndex] = useState(0)
-
-    useEffect(() => {
-        api.get("/applications")
-            .then(r => setApplications(r ? r.data.filter(a => a.state === "JOB_OFFER_ACCEPTED_BY_STUDENT"
-                && a.offer.employer.id === AuthenticationService.getCurrentUser().id) : []))
-    }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
     useEffect(() => {
         api.get("/contract")
@@ -25,32 +18,28 @@ export default function HiredStudentList() {
                 && c.studentApplication.offer.employer.id === AuthenticationService.getCurrentUser().id) : []))
     }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
-    useEffect(() => {
-        api.get("/offers")
-            .then(r => setCurrentOffer(r ? r.data.filter(o => o.employer.id === AuthenticationService.getCurrentUser().id) : []))
-    }, []) // eslint-disable-line react-hooks/exhaustive-deps
-
     return <div style={{height: "100%"}}>
         <PdfSelectionViewer documents={contracts.map(c => c.file)} title={"Les étudiants embauchés"}>
-            {(i, setCurrent) => <div key={i}>
-                <button
-                    type={"button"}
-                    className={[classes.buttonDiv, i === currentIndex ? classes.fileButton : null].join(' ')}
-                    autoFocus={i === 0}
-                    onClick={() => {
-                        setCurrentIndex(i)
-                        setCurrent(i)
-                    }}>
-                    <Typography color={"textPrimary"} variant={"body1"} display={"inline"}>
-                        {contracts[i].studentApplication.student.firstName} {contracts[i].studentApplication.student.lastName}
-                    </Typography>
-                </button>
-                <Typography
-                    variant={"body2"}>
-
-                </Typography>
-                <hr/>
-            </div>}
+            {(i, setCurrent) =>
+                <div key={i}>
+                    <button
+                        type={"button"}
+                        className={[classes.linkButton, i === currentIndex ? classes.fileButton : null].join(' ')}
+                        autoFocus={i === 0}
+                        onClick={() => {
+                            setCurrentIndex(i)
+                            setCurrent(i)
+                        }}>
+                        <Typography color={"textPrimary"} variant={"body1"} display={"inline"}>
+                            {contracts[i].studentApplication.student.firstName} {contracts[i].studentApplication.student.lastName}
+                        </Typography>
+                    </button>
+                    {currentIndex === i && <div>
+                        <OfferDetails offer={contracts[i].studentApplication.offer}/>
+                    </div>}
+                    <hr/>
+                </div>
+            }
         </PdfSelectionViewer>
     </div>
 }
