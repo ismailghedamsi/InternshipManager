b045aa95f377dd7d7e35ac2a7b36c4d8ba1970b9
Simon Longpré-Landry <simonllandry@hotmail.com>
2020-11-27 19:34:20 -0500

    EQ3-179: Extrait le PdfModal et resizing automatique

--- react-frontend/src/Components/Manager/StudentStatus.jsx
+++ react-frontend/src/Components/Manager/StudentStatus.jsx
@@ -1,14 +1,11 @@
 import {Divider, Typography} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
 import Grid from "@material-ui/core/Grid";
 import * as PropTypes from "prop-types";
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {useApi, useDateParser, useModal} from "../../Services/Hooks";
 import OfferDetails from "../Utils/OfferDetails";
-import PdfDocument from "../Utils/PDF/PdfDocument";
+import PdfModal from "../Utils/PdfModal"
 import useStyles from "../Utils/Style/useStyles";
 
 const applicationAcceptedStates = [
@@ -115,7 +112,7 @@ OfferStatus.propTypes = {
     classes: PropTypes.any,
     onClick: PropTypes.func,
     offer: PropTypes.any,
-    currentStudent: PropTypes.any,
+    currentStudent: PropTypes.any
 }
 
 function StudentApplicationDetails({student}) {
@@ -243,7 +240,6 @@ export default function StudentStatus() {
     const [currentSubtab, setCurrentSubtab] = useState(0)
     const [currentDoc, setCurrentDoc] = useState("")
     const [isPdfOpen, openPdf, closePdf] = useModal()
-    const pdfContainer = useRef()
 
     useEffect(() => {
         api.get("students").then(resp => setStudents(resp ? resp.data : []))
@@ -327,15 +323,9 @@ export default function StudentStatus() {
                              }}/>
             ) : " L'étudiant n'a accès à aucune offre de stage")}
         </Grid>
-        <Dialog open={isPdfOpen} onClose={closePdf} maxWidth={"xl"}>
-            <DialogContent className={classes.viewbox} ref={pdfContainer}>
-                <PdfDocument document={currentDoc} container={pdfContainer}/>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={closePdf} color="primary">
-                    Fermer
-                </Button>
-            </DialogActions>
-        </Dialog>
+        <PdfModal open={isPdfOpen}
+                  onClose={closePdf}
+                  document={currentDoc}
+        />
     </Grid>
 }
