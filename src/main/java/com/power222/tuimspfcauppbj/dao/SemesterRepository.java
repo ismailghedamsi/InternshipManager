23aea409162e76257801be46d97c0c1d82de0bd3
Andrei <andrei.belkin.0@protonmail.com>
2020-12-05 22:40:55 -0500

    EQ3-add-bootstrapping : Bootstrapped 30 students & del an extra space in SemesterRepo

--- src/main/java/com/power222/tuimspfcauppbj/dao/SemesterRepository.java
+++ src/main/java/com/power222/tuimspfcauppbj/dao/SemesterRepository.java
@@ -14,7 +14,7 @@ public class SemesterRepository {
 
     @SuppressWarnings({"unchecked", "SqlDialectInspection", "SqlNoDataSourceInspection"})
     public List<String> findAll() {
-        return em.createNativeQuery("SELECT DISTINCT semester from (SELECT semester FROM INTERNSHIP_OFFER  " +
+        return em.createNativeQuery("SELECT DISTINCT semester from (SELECT semester FROM INTERNSHIP_OFFER " +
                 "union all SELECT semester FROM INTERVIEW " +
                 "union all SELECT semester FROM STUDENT_APPLICATION " +
                 "union all SELECT semesters FROM EMPLOYER_SEMESTERS " +
